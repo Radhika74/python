ce5967f5f2ffb0a99eea25e09888cf229fb45013
Radhika Thakur <radhikathakur1103@gmail.com>
2025-01-26 22:42:59 +0530

    Function and types

--- 019_function_type_argu.py
+++ 019_function_type_argu.py
@@ -4,7 +4,7 @@ def average(a,b):
 average(4,6)
 #default argument 
 def name(fname,lname="John",mname="whatson"):
-   print(fname,mname,lname)
+    print(fname,mname,lname)
 name("Amyy")
 #keyword argument 
 def name1(fname,mname,lname):
@@ -16,22 +16,22 @@ def name2(fname,mname,lname):
 name2("Peter","Ego","Quill")
 #variable length argument
 def average1(*numbers):
-   print(type(numbers))
-   sum=0
-   for i in numbers:
-     sum=sum+i
+    print(type(numbers))
+    sum=0
+    for i in numbers:
+      sum=sum+i
 
-   print("Average is:",sum/len(numbers))
+    print("Average is:",sum/len(numbers))
 
 average (5,6)
 #return statement
 def average2(*numbers):
-   print(type(numbers))
-   sum=0
-   for i in numbers:
-     sum=sum+i
+    print(type(numbers))
+    sum=0
+    for i in numbers:
+      sum=sum+i
 
-   return sum/len(numbers)
+    return sum/len(numbers)
 
 c=average2(5,6,7,1)
 print(c)
