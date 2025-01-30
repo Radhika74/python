885ebf1a2bf6d83f667c87e82fb5b4f76a4d9591
Radhika Thakur <radhikathakur1103@gmail.com>
2025-01-30 07:22:40 +0530

    Excercise 3 KBC

--- 025_excercise3KBC.py
+++ 025_excercise3KBC.py
@@ -6,18 +6,18 @@ print("Q1.Who is the father of computer?")
 #code that display options to user
 #using match case and list 
 match list1:
- case 1:
-     print("1.Charles Babbage")
- case 2:
-     print("2.Albert Einstein")
- case 3:
-     print("3.Newton")
- case 4:
-     print("4.Thomas Alva Edison")
- case _ :
-     print("The write answer is:",)
-   
-#next questions 
+    case 1:
+        print("1.Charles Babbage")
+    case 2:
+        print("2.Albert Einstein")
+    case 3:
+        print("3.Newton")
+    case 4:
+        print("4.Thomas Alva Edison")
+    case _ :
+        print("The write answer is:",)
+        
+#next questions
 
 print("Q2.Who discover programming language concept ")
     
