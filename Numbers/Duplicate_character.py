6206c55511580d6c6a3622541c201531fc9e9835
Radhika Thakur <radhikathakur1103@gmail.com>
2025-02-18 23:55:06 +0530

    Prime Numbers

--- Numbers/Duplicate_character.py
+++ Numbers/Duplicate_character.py
@@ -4,7 +4,6 @@ def remove_duplicate(your_str):
         if char not in result:
             result += char
     return result
-
 user_input = input('what is your string:')
 no_duplicate = remove_duplicate(user_input)
 print('Without duplicate: ', no_duplicate)
--- Numbers/find_prime_no_user_given.py
+++ Numbers/find_prime_no_user_given.py
@@ -3,14 +3,14 @@ def is_prime(num):
         if (num % i) == 0:
             return False
         return True
- 
+        
 def all_primes(num):
     primes = []
     for n in range(2,num+1):
         if is_prime(n) is True:
             primes.append(n)
     return primes
- 
+
 num = int(input("Enter upper limit: "))
 primes = all_primes(num)
 print(primes)
