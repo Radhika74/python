cbdd4fb46e1f363ce934345ecd1d2309de2062de
Radhika Thakur <radhikathakur1103@gmail.com>
2025-03-24 22:08:28 +0530

    Meetings

---
+++ files/meetings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countDays(self, days, meetings):
+        result, end=0,0
+        for start, e in sorted(meetings)+[[days+1]*2]:
+            result += max(0,start-end-1)
+            end = max(end,e)
+        return result
+    
+days = 100
+meetings = [[0,30],[5,10],[15,20]]
+print(Solution().countDays(days, meetings))
