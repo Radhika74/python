64fbea33db853b4ba814ae374fc12b7642db327a
Radhika Thakur <radhikathakur1103@gmail.com>
2025-05-12 23:28:59 +0530

    solved 3 even no problem

---
+++ files/3-digit_even_no.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findEvenNumbers(self, digits):
+        from collections import Counter
+        freq = Counter(digits)
+        res = []
+        for num in range(100, 1000, 2):
+            parts = [num // 100, (num // 10) % 10, num % 10]
+            count = Counter(parts)
+            if all(freq[d] >= count[d] for d in count):
+                res.append(num)
+        return res
+# Example usage
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+sol = Solution()
+result = sol.findEvenNumbers(digits)
+print(result)
