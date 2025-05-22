cd7d59ef5664c395ea26b68d4620807f27bb17f6
Radhika Thakur <radhikathakur1103@gmail.com>
2025-05-22 08:40:20 +0530

    transformation in arrays

---
+++ files/zero_arr_transformation-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
+        queries.sort(key=lambda x: x[0])
+        heap = []
+        deltaArray = [0] * (len(nums) + 1)
+        operations = 0
+        j = 0
+        for i, num in enumerate(nums):
+            operations += deltaArray[i]
+            while j < len(queries) and queries[j][0] == i:
+                heappush(heap, -queries[j][1])
+                j += 1
+            while operations < num and heap and -heap[0] >= i:
+                operations += 1
+                deltaArray[-heappop(heap) + 1] -= 1
+            if operations < num:
+                return -1
+        return len(heap)
+
+if __name__ == "__main__":
+    sol = Solution()
+    # Test case 1
+    nums = [1, 2, 3]
+    queries = [[0, 1], [1, 2], [2, 3]]
+    print(sol.maxRemoval(nums, queries))  # Expected output: 3
+
